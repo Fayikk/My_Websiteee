20c1409153619f4563e7160bbf56e6c662311ed7
Fayik <veznedaroglufayik2@gmail.com>
2023-02-01 23:55:16 +0300

    Ajax with get Data

---
+++ Website/Controllers/Experience2Controller.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Website.Controllers
+{
+    public class Experience2Controller : Controller
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new DataAccessLayer.EntityFramework.EfExperienceDal());
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListExperience()
+        {
+            var values = JsonConvert.SerializeObject(experienceManager.TGetList());
+            return Json(values);
+        }
+
+        [HttpPost]
+        public IActionResult AddExperience(Experience p)
+        {
+            experienceManager.TAdd(p);
+            var values = JsonConvert.SerializeObject(p);
+            return Json(values);
+        }
+    }
+}
